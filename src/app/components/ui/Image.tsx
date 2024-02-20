5d5dd647c560085f5f65144fb97dd02fbc812e64
Davi Damião <daviddperera10@gmail.com>
2024-02-20 04:25:15 +0000

    generic image component

---
+++ src/app/components/ui/Image.tsx
@@ -0,0 +1,35 @@
+"use client";
+import NextImage from "next/image";
+import { HTMLAttributes, useState } from "react";
+
+interface ImgProps extends HTMLAttributes<HTMLImageElement> {
+  src: string;
+  alt?: string;
+  className?: string;
+  priority?: boolean;
+}
+
+export default function Image({
+  priority,
+  className,
+  src,
+  ...props
+}: ImgProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  return (
+    <NextImage
+      {...props}
+      src={src}
+      alt=""
+      priority={priority}
+      
+      onLoad={() => setIsLoading(false)}
+      className={`${className ? className : ""} ${
+        isLoading ? "brightness-50" : ""
+      }  duration-100`}
+      width={100}
+      height={100}
+    />
+  );
+}
