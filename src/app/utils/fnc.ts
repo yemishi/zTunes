2ebbbbd70ec666c12a71df002375aa706b29753c
Davi Damião <daviddperera10@gmail.com>
2024-03-05 01:30:38 +0000

    generics functions added

--- src/app/utils/fnc.ts
+++ src/app/utils/fnc.ts
@@ -1,3 +1,4 @@
+import { ErrorType } from "@/types/response";
 import { usePathname } from "next/navigation";
 import Vibrant from "node-vibrant";
 
@@ -18,11 +19,30 @@ export const urlMatch = (path: string) => {
 export const getVibrantColor = async (img: string) => {
   const palette = await Vibrant.from(img).getPalette();
   return {
-    default: `rgb(${palette.Vibrant?.rgb.join(",")})`,
+    default: `rgb(${palette.DarkMuted?.rgb.join(",")})`,
     light: `rgb(${palette.LightVibrant?.rgb.join(",")})`,
     dark: `rgb(${palette.DarkVibrant?.rgb.join(",")})`,
     mutedDark: `rgb(${palette.DarkMuted?.rgb.join(",")})`,
     mutedLight: `rgb(${palette.LightMuted?.rgb.join(",")})`,
     muted: `rgb(${palette.Muted?.rgb.join(",")})`,
   };
+};
+
+export const removeFromPlaylist = async (
+  selected: {
+    songId: any;
+    createdAt: Date | undefined;
+  },
+  playlistId: string
+) => {
+  const body = {
+    selected,
+    toRemove: true,
+    id: playlistId,
+  };
+  const data: Promise<ErrorType> = fetch(`/api/playlist`, {
+    method: "PATCH",
+    body: JSON.stringify(body),
+  }).then((res) => res.json());
+  return data;
 };
