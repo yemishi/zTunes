ed597d9a513bc0a75af97c4123056236aeec522f
Davi Damião <daviddperera10@gmail.com>
2024-03-05 01:31:05 +0000

    new global context

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,7 +1,26 @@
 "use client";
 
+import { SongType } from "@/types/response";
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { createContext, useContext, useState } from "react";
+
+interface PlayerContextProps {
+  player: SongType[] | undefined;
+  setPlayer: React.Dispatch<React.SetStateAction<SongType[] | undefined>>;
+  currSong: number | undefined;
+  setCurrSong: React.Dispatch<React.SetStateAction<number | undefined>>;
+}
+
+const PlayerContext = createContext<PlayerContextProps | undefined>(undefined);
+
+export const usePlayerContext = () => {
+  const context = useContext(PlayerContext);
+  if (!context) {
+    throw new Error("usePlayerContext must be used within a PlayerProvider");
+  }
+  return context;
+};
 
 export default function Provider({
   children,
@@ -10,5 +29,15 @@ export default function Provider({
   children: React.ReactNode;
   session: Session | null;
 }) {
-  return <SessionProvider  session={session}>{children}</SessionProvider>;
+  const [currSong, setCurrSong] = useState<number>();
+  const [player, setPlayer] = useState<SongType[]>();
+  return (
+    <SessionProvider session={session}>
+      <PlayerContext.Provider
+        value={{ currSong, setCurrSong, player, setPlayer }}
+      >
+        {children}
+      </PlayerContext.Provider>
+    </SessionProvider>
+  );
 }
