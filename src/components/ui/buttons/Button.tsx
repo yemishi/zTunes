66e2cb5af2f1109e56956217368576df9691dbb5
yemishi <daviddperera10@gmail.com>
2025-05-02 22:20:13 +0100

    updated ui

--- src/components/ui/buttons/Button.tsx
+++ src/components/ui/buttons/Button.tsx
@@ -7,24 +7,18 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   isLoading?: boolean;
 }
 
-export default function Button({
-  className,
-  isLoading,
-  asChild,
-  ...props
-}: ButtonProps) {
+export default function Button({ className, isLoading, asChild, ...props }: ButtonProps) {
   const Component = asChild ? Slot : "button";
-  const defaultBackDrop = className?.includes("bg-transparent")
-    ? ""
-    : "backdrop-brightness-50";
+  const defaultBackDrop = className?.includes("bg-transparent") ? "" : "backdrop-brightness-50";
   const defaultBg = !className?.includes("bg") ? "bg-amber-500" : "";
 
   return (
     <Component
       {...props}
-      className={`${className ? className : ""
-        } ${defaultBackDrop} ${defaultBg}  px-4 py-2 rounded-full border border-white border-opacity-30 font-kanit hover:backdrop-brightness-150
-      disabled:animate-pulse duration-200  bg-opacity-75 hover:bg-opacity-85`}
+      className={`${
+        className ? className : ""
+      } ${defaultBackDrop} ${defaultBg}  cursor-pointer px-4 py-2 rounded-full border border-white border-opacity-30 font-kanit hover:backdrop-brightness-150
+      disabled:animate-pulse duration-200 bg-opacity-75 hover:bg-opacity-85 border-none`}
     />
   );
 }
