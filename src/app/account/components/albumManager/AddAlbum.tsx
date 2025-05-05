7fc91c07c8f807445e6d2ab49dbe8f89a2c60005
yemishi <daviddperera10@gmail.com>
2025-05-05 20:56:46 +0100

    correct props passed to Input and AlbumForm component

--- src/app/account/components/albumManager/AddAlbum.tsx
+++ src/app/account/components/albumManager/AddAlbum.tsx
@@ -9,28 +9,13 @@ import { useState } from "react";
 export default function AddAlbum({ artistId }: { artistId: string }) {
   const [title, setTitle] = useState<string>("");
   const { close, setChildren } = useTempOverlay();
-  const Form = () => (
-    <AlbumForm artistId={artistId} initialTitle={title} onclose={close} />
-  );
+  const Form = () => <AlbumForm artistId={artistId} title={title} onclose={close} />;
 
   return (
     <>
-      <DivAnimated
-        oneSide
-        onClick={(e) => e.stopPropagation()}
-        className=" flex items-center px-2"
-      >
-        <Input
-          label=""
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="New album name"
-          classNameInput="bg-transparent border-gray-300 border-b-2"
-        />
-        <Button
-          onClick={() => setChildren(Form)}
-          className="ml-auto rounded-lg text-sm text-black"
-        >
+      <DivAnimated oneSide onClick={(e) => e.stopPropagation()} className=" flex items-center px-2">
+        <Input label="New album name" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <Button onClick={() => setChildren(Form)} className="ml-auto rounded-lg text-sm text-black">
           New
         </Button>
       </DivAnimated>
