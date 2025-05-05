f7c71a09d73faeee620f331695fa6fc34961956b
yemishi <daviddperera10@gmail.com>
2025-05-05 20:52:16 +0100

    improved ui and cleaned the code

--- src/components/register/confirmInfo.tsx
+++ src/components/register/confirmInfo.tsx
@@ -8,26 +8,26 @@ type PropsType = {
   response?: ErrorType;
 };
 
-//todo improve ui
-
 export default function ConfirmInfo({ email, name, bDay, response }: PropsType) {
   const [day, month, year] = bDay.split("/");
   const formattedDate = format(String(Number(month) + 1), "LLLL");
 
   return (
-    <div className="flex flex-col h-full gap-5">
-      <div className="flex border-l-4 border-orange-600 pl-1 flex-col text-left gap-1 font-poppins text-lg mt-4 ml-4">
-        <p>{name}</p>
-        <p>{`${day} of ${formattedDate} of ${year}`}</p>
-        <p>{email}</p>
+    <div className="flex flex-col h-full gap-6 p-4">
+      <div className="flex flex-col gap-1 p-3 text-left border-l-4 border-orange-600 pl-4 font-poppins text-base sm:text-lg bg-orange-50/10 rounded-md shadow-sm">
+        <p className="font-semibold text-orange-200">{name}</p>
+        <p className="text-orange-100">{`${day} of ${formattedDate} of ${year}`}</p>
+        <p className="text-orange-100">{email}</p>
       </div>
 
       <div
         className={`${
-          response ? "bg-opacity-35" : "bg-opacity-20"
-        } font-kanit text-xl px-3 py-5 rounded-lg border border-white border-opacity-50 mt-3`}
+          response ? " border-green-300/50" : " border-white/50"
+        } font-kanit text-lg sm:text-xl px-5 py-6 bg-white/20 rounded-xl border transition-colors `}
       >
-        <p className="px-3 font-medium">{response ? response.message : "If everything is right click below"}</p>
+        <p className="px-1 font-medium text-white">
+          {response ? response.message : "If everything looks good, click below to continue."}
+        </p>
       </div>
     </div>
   );
