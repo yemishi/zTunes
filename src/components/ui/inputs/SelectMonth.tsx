ac30a886135d28d2555edb74bcfb83357d2876ca
yemishi <daviddperera10@gmail.com>
2025-05-05 20:54:49 +0100

    improved ui

--- src/components/ui/inputs/SelectMonth.tsx
+++ src/components/ui/inputs/SelectMonth.tsx
@@ -8,24 +8,23 @@ interface SelectType extends SelectHTMLAttributes<HTMLSelectElement> {
 const SelectMonth = forwardRef<HTMLSelectElement, SelectType>((props, ref) => {
   const { error, ...rest } = props;
 
-  const months = Array.from({ length: 12 }, (_, index) =>
-    format(new Date(2024, index, 1), "MMMM")
-  );
+  const months = Array.from({ length: 12 }, (_, index) => format(new Date(2024, index, 1), "MMMM"));
   return (
     <select
       ref={ref}
       {...rest}
-      className={`inputForm bg-black bg-opacity-45 border border-white border-opacity-55 ${
-        error && "border-red-500"
-      }`}
       defaultValue={rest.defaultValue || ""}
+      className={`appearance-none rounded-full px-4 py-2 pr-10 border-2 border-l-1 transition-colors 
+      ${error ? "border-red-400" : rest.value ? "border-blue-300" : "border-white"}
+      outline-none focus:border-amber-500
+    `}
     >
       <option disabled value="">
         Month
       </option>
       {months.map((value, index) => {
         return (
-          <option value={index} key={`${value}_${index}`}>
+          <option className="bg-black-500" value={index} key={`${value}_${index}`}>
             {value}
           </option>
         );
