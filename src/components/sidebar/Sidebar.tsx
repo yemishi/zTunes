9e997114cc9f9bac86bbf00e548f8443a14c41b5
yemishi <daviddperera10@gmail.com>
2025-05-07 19:49:22 +0100

    now list lists is inside of a array

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { urlMatch } from "@/utils/helpers";
 import { BiLibrary } from "react-icons/bi";
 import { PiHouseLight, PiMagnifyingGlassLight } from "react-icons/pi";
@@ -13,14 +14,32 @@ export default function Sidebar() {
   const hidden = () =>
     urlMatch("login") || urlMatch("sign-up") || urlMatch("validation") || urlMatch("password-reset") || isMobile();
   if (hidden()) return;
+  const liMapped = [
+    { Icon: PiHouseLight, to: "home" },
+    { Icon: PiMagnifyingGlassLight, to: "search" },
+    { Icon: BiLibrary, to: "myLib", name: "Library" },
+  ];
 
   return (
     <div className="fixed left-0 top-0 w-64 lg:w-72 2xl:w-80  min-[2000px]:w-96 h-full pb-20 flex flex-col p-2 gap-2 bg-black">
       <Suspense fallback={<div className="spinner" />}>
         <SessionPanel />
       </Suspense>
+      <ul className="flex flex-col gap-2 p-2 bg-black-700 rounded-lg">
+        {liMapped.map(({ Icon, to, name }, i) => {
+          return (
+            <Link
+              key={`${to}_${i}`}
+              href={`/${to}`}
+              className="flex gap-2 p-3 items-center font-bold opacity-50 hover:opacity-100 active:bg-black rounded-lg transition-all "
+            >
+              <Icon className={`size-7 ${to === "myLib" ? "" : "stroke-[8px]"}`} />
+              <span className="first-letter:uppercase">{name || to}</span>
+            </Link>
+          );
+        })}
+      </ul>
 
-      <LinkList />
       <div className="flex flex-col rounded-lg flex-1 bg-black-700 p-2 overflow-y-auto">
         <Suspense fallback={<div className="spinner" />}>
           <LibInfo />
@@ -29,30 +48,3 @@ export default function Sidebar() {
     </div>
   );
 }
-
-const LinkList = () => {
-  const liStyle =
-    "flex gap-2 p-3 items-center font-bold text-white opacity-50 duration-100 hover:opacity-100 active:bg-black rounded-lg";
-  return (
-    <ul className="flex flex-col gap-2 p-2 bg-black-700 rounded-lg">
-      <li>
-        <Link href="/home" className={liStyle}>
-          <PiHouseLight className="size-7 stroke-[8px]" />
-          <span>Home</span>
-        </Link>
-      </li>
-      <li>
-        <Link href="/search" className={liStyle}>
-          <PiMagnifyingGlassLight className="size-7" />
-          <span>Search</span>
-        </Link>
-      </li>
-      <li>
-        <Link href="/myLib" className={liStyle}>
-          <BiLibrary className="size-7" />
-          <span>Library</span>
-        </Link>
-      </li>
-    </ul>
-  );
-};
