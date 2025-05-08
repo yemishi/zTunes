8f22a7264b40f793a1ec3cc1d21f1ef64daaedcd
yemishi <daviddperera10@gmail.com>
2025-05-08 20:07:41 +0100

    refactored document for clarity

--- src/components/ui/inputs/DateFields.tsx
+++ src/components/ui/inputs/DateFields.tsx
@@ -6,24 +6,25 @@ import { lastDayOfMonth, format } from "date-fns";
 interface PropsType extends HTMLAttributes<HTMLDivElement> {
   setValue: (fieldName: string, value: string) => void;
   values: {
-    bDay: string;
-    bMonth: string;
-    bYear: string;
+    day: string;
+    month: string;
+    year: string;
   };
   errors: Record<string, string | null>;
+  optionsClassName?: string;
   isLoading?: boolean;
 }
 
-export default function DateFields({ errors, values, isLoading, setValue, ...props }: PropsType) {
-  const { bDay, bMonth, bYear } = values;
-  const { className } = props;
+export default function DateFields({ errors, values, isLoading, setValue, optionsClassName, ...props }: PropsType) {
+  const { day, month, year } = values;
+  const { className = "" } = props;
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
-    setValue(name, name === "day" ? handleLimitDay(value) : name === "bYear" ? handleLimitYear(value) : value);
+    setValue(name, name === "day" ? handleLimitDay(value) : name === "year" ? handleLimitYear(value) : value);
   };
 
-  const lastDay = format(lastDayOfMonth(new Date(Number(bYear), Number(bMonth))), "d");
+  const lastDay = format(lastDayOfMonth(new Date(Number(year), Number(month))), "d");
 
   const handleLimitDay = (value: string) => {
     if (Number(value) >= Number(lastDay)) {
@@ -43,44 +44,46 @@ export default function DateFields({ errors, values, isLoading, setValue, ...pro
     <div className="grid grid-cols-[1fr_2fr_1.5fr] gap-3 relative">
       <Input
         disabled={isLoading}
+        className={className}
         required
         label="Day"
         id="day"
         placeholder="dd"
         type="number"
-        autoComplete="bDay"
+        autoComplete="day"
         inputMode="numeric"
         disableErrorMsg
-        error={errors?.bDay || ""}
-        value={bDay}
-        name="bDay"
+        error={errors?.day || ""}
+        value={day}
+        name="day"
         onChange={handleInput}
       />
 
       <SelectMonth
-        className={className ? className : ""}
+        optionsClassName={optionsClassName}
+        className={className}
         onChange={handleInput}
         disabled={isLoading}
-        error={errors?.bDay || ""}
-        id="bMonth"
-        name="bMonth"
+        error={errors?.day || ""}
+        id="month"
+        name="month"
         required
       />
 
       <Input
-        className={className ? className : ""}
+        className={className}
         required
         disabled={isLoading}
         label="Year"
         placeholder="yyyy"
-        name="bYear"
+        name="year"
         inputMode="numeric"
         type="number"
-        value={bYear}
+        value={year}
         disableErrorMsg
-        error={errors?.bDay || ""}
+        error={errors?.day || ""}
         onChange={handleInput}
-        autoComplete="bYear"
+        autoComplete="year"
       />
     </div>
   );
