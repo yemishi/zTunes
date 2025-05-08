bca53c5024f47f5ddff04d3662f19e23762a4d64
yemishi <daviddperera10@gmail.com>
2025-05-08 20:07:00 +0100

    renamed props for clarity

--- src/components/ui/inputs/SelectMonth.tsx
+++ src/components/ui/inputs/SelectMonth.tsx
@@ -1,12 +1,14 @@
+import { cleanClasses } from "@/utils/helpers";
 import { format } from "date-fns";
 import { SelectHTMLAttributes, forwardRef } from "react";
 
 interface SelectType extends SelectHTMLAttributes<HTMLSelectElement> {
-  error?: string | undefined;
+  error?: string;
+  optionsClassName?: string;
 }
 
 const SelectMonth = forwardRef<HTMLSelectElement, SelectType>((props, ref) => {
-  const { error, ...rest } = props;
+  const { error, className, optionsClassName, ...rest } = props;
 
   const months = Array.from({ length: 12 }, (_, index) => format(new Date(2024, index, 1), "MMMM"));
   return (
@@ -14,9 +16,12 @@ const SelectMonth = forwardRef<HTMLSelectElement, SelectType>((props, ref) => {
       ref={ref}
       {...rest}
       defaultValue={rest.defaultValue || ""}
-      className={`appearance-none rounded-full px-4 py-2 pr-10 border-2 border-l-1 transition-colors 
+      className={`${cleanClasses(
+        className,
+        "appearance-none rounded-full px-4 py-2 pr-10 border-2 border-l-1 transition-colors outline-none focus:border-amber-500"
+      )}
       ${error ? "border-red-400" : rest.value ? "border-blue-300" : "border-white"}
-      outline-none focus:border-amber-500
+   
     `}
     >
       <option disabled value="">
@@ -24,7 +29,7 @@ const SelectMonth = forwardRef<HTMLSelectElement, SelectType>((props, ref) => {
       </option>
       {months.map((value, index) => {
         return (
-          <option className="bg-black-500" value={index} key={`${value}_${index}`}>
+          <option className={cleanClasses(optionsClassName, "bg-black-500")} value={index} key={`${value}_${index}`}>
             {value}
           </option>
         );
