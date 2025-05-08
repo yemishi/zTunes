8f4b3a50f1634b332a94fc644cc0fb39c0c8cdcb
yemishi <daviddperera10@gmail.com>
2025-05-08 20:08:09 +0100

    fixed an error at setCateries state

--- src/components/ui/inputs/AddCategories.tsx
+++ src/components/ui/inputs/AddCategories.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Input from "./Input";
 import Button from "../buttons/Button";
 
@@ -11,27 +11,26 @@ interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
 
 export default function AddCategories({ setCategories, categories, label, placeHolder, ...props }: InputProps) {
   const { className, ...rest } = props;
-  const inputRef = useRef<HTMLInputElement>(null);
-
+  const [value, setValue] = useState("");
   const remove = (category: string) => setCategories(categories.filter((item) => item !== category));
 
   const add = () => {
-    if (inputRef.current && inputRef.current.value.trim() && categories.length <= 5) {
-      setCategories([...categories, inputRef.current.value.replaceAll(" ", "")]);
-      inputRef.current.value = "";
-    }
+    if (categories.includes(value)) return;
+    setCategories([...categories, value]);
+    setValue("");
   };
 
   return (
     <div {...props} className="flex flex-col gap-4 sm:grid-cols-2">
       <div className={`flex gap-4  ${className ? className : ""}`}>
         <Input
-          ref={inputRef}
+          onChange={(e) => setValue(e.target.value.trim())}
+          value={value}
           label={label || "Category"}
           placeholder={placeHolder || "Category"}
-          className="bg-transparent backdrop-brightness-150 border-neutralDark-400"
+          className="bg-transparent backdrop-brightness-150 border-neutralDark-400 !border-black"
         />
-        <Button type="button" className="bg-transparent mt-auto" onClick={add}>
+        <Button type="button" onClick={add}>
           Add
         </Button>
       </div>
@@ -39,11 +38,11 @@ export default function AddCategories({ setCategories, categories, label, placeH
       <div className="flex flex-wrap gap-3">
         {categories.map((category, index) => {
           return (
-            <span className="relative py-2  px-6 rounded-lg bg-orange-600 bg-opacity-50" key={`${category}_${index}`}>
+            <span className="relative py-2  px-6 rounded-lg bg-white bg-opacity-50" key={`${category}_${index}`}>
               <span>{category}</span>
               <span
                 onClick={() => remove(category)}
-                className="absolute top-0 right-0 size-5 bg-black cursor-pointer rounded-bl-lg rounded-tr-lg flex justify-center items-center font-mono"
+                className="absolute top-0 right-0 size-5 bg-black text-white cursor-pointer rounded-bl-lg rounded-tr-lg flex justify-center items-center font-mono"
               >
                 x
               </span>
