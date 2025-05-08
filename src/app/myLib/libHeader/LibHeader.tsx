21a9b584042c98a453810a96651b1c3fc9716236
yemishi <daviddperera10@gmail.com>
2025-05-08 20:08:52 +0100

    refactored document for clarity

--- src/app/myLib/libHeader/LibHeader.tsx
+++ src/app/myLib/libHeader/LibHeader.tsx
@@ -3,9 +3,8 @@
 import { useSession } from "next-auth/react";
 import Image from "@/components/ui/custom/Image";
 import { FaPlus } from "react-icons/fa6";
-import NewPlaylistForm from "@/components/form/NewPlaylistForm";
 import { useState } from "react";
-import Modal from "@/components/modal/Modal";
+import { Modal, PlaylistForm } from "@/components";
 
 export default function LibHeader() {
   const { data: session } = useSession();
@@ -17,19 +16,16 @@ export default function LibHeader() {
     <div className="w-full bg-[#121212] p-4 sticky top-0 flex items-center gap-2">
       {user?.picture && <Image src={user.picture} className="size-12 rounded-full" />}
       {isForm && (
-        <Modal
-          className="w-screen h-screen overflow-x-hidden overflow-y-clip max-w-3xl md:h-[900px] md:rounded-lg mx-auto my-auto bg-gray-300 p-4"
-          onClose={() => setIsForm(false)}
-        >
-          <NewPlaylistForm username={user?.name as string} onclose={() => setIsForm(false)} />;
+        <Modal className="modal-container" onClose={() => setIsForm(false)}>
+          <PlaylistForm username={user?.name as string} onClose={() => setIsForm(false)} />;
         </Modal>
       )}
       <span className="font-montserrat font-semibold text-xl">Your library</span>
       <button
         onClick={() => setIsForm(true)}
-        className="size-10 p-3 md:hidden bg-black-450 hover:bg-opacity-75 active:scale-105 duration-150 rounded-full ml-auto"
+        className="size-10 p-3 md:hidden bg-black-300 brightness-80 cursor-pointer hover:brightness-100 active:scale-105 duration-150 rounded-full ml-auto transition-all"
       >
-        <FaPlus className="text-zinc-400 h-full w-full" />
+        <FaPlus />
       </button>
     </div>
   );
