f82cbe3bcf9185c53f3d9a1775494f5975415497
yemishi <daviddperera10@gmail.com>
2025-05-10 20:35:22 +0100

    changed UserSessionPanel and UserLibPanel imports

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -8,8 +8,9 @@ import { Suspense, lazy } from "react";
 import isMobile from "@/utils/isMobile";
 import Link from "next/link";
 
-const SessionPanel = lazy(() => import("./SessionPanel"));
-const LibInfo = lazy(() => import("./LibInfo"));
+const UserSessionPanel = lazy(() => import("./userSessionPanel/SessionPanel"));
+const UserLibPanel = lazy(() => import("./userLibPanel/UserLibPanel"));
+
 export default function Sidebar() {
   const hidden = () =>
     urlMatch("login") || urlMatch("sign-up") || urlMatch("validation") || urlMatch("password-reset") || isMobile();
@@ -23,7 +24,7 @@ export default function Sidebar() {
   return (
     <div className="fixed left-0 top-0 w-64 lg:w-72 2xl:w-80  min-[2000px]:w-96 h-full pb-20 flex flex-col p-2 gap-2 bg-black">
       <Suspense fallback={<div className="spinner" />}>
-        <SessionPanel />
+        <UserSessionPanel />
       </Suspense>
       <ul className="flex flex-col gap-2 p-2 bg-black-700 rounded-lg">
         {liMapped.map(({ Icon, to, name }, i) => {
@@ -42,7 +43,7 @@ export default function Sidebar() {
 
       <div className="flex flex-col rounded-lg flex-1 bg-black-700 p-2 overflow-y-auto">
         <Suspense fallback={<div className="spinner" />}>
-          <LibInfo />
+          <UserLibPanel />
         </Suspense>
       </div>
     </div>
