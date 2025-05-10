beecd67c85cf4edbfe2d345b6f9800fa7327cece
yemishi <daviddperera10@gmail.com>
2025-05-10 21:22:23 +0100

    export Slider component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as PlaylistForm } from "./playlistForm/PlaylistForm";
 export { default as Modal } from "./modal/Modal";
 export { default as PopConfirm } from "./popConfirm/PopConfirm";
 export { default as ErrorWrapper } from "./errorWrapper/ErrorWrapper";
+export { default as Slider } from "./slider/Slider";
