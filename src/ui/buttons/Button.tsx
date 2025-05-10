d38f38aac42cd4c9da1ae6b6a26911ea727cae90
yemishi <daviddperera10@gmail.com>
2025-05-10 20:35:49 +0100

    accepts a prop to return as Link instead of button

--- src/ui/buttons/Button.tsx
+++ src/ui/buttons/Button.tsx
@@ -1,24 +1,29 @@
-import { ButtonHTMLAttributes, ReactNode } from "react";
+"use client";
 
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import Link from "next/link";
 import { cleanClasses } from "@/utils/helpers";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   isLoading?: boolean;
   children?: ReactNode;
+  href?: string;
 }
 
-export default function Button({ children, isLoading, ...props }: ButtonProps) {
+export default function Button({ children, isLoading, href, ...props }: ButtonProps) {
   const { className, ...rest } = props;
-  return (
-    <button
-      {...rest}
-      className={cleanClasses(
-        className,
-        `cursor-pointer px-4 py-2 rounded-full text-black bg-amber-500 font-kanit hover:brightness-110 active:scale-90 transition-all disabled:grayscale ${
-          isLoading ? "animate-pulse pointer-events-none" : ""
-        }`
-      )}
-    >
+  const baseClasses = cleanClasses(
+    className,
+    `cursor-pointer px-4 py-2 rounded-full text-black bg-amber-500 font-kanit hover:brightness-110 active:scale-90 transition-all disabled:grayscale ${
+      isLoading ? "animate-pulse pointer-events-none" : ""
+    }`
+  );
+  return href ? (
+    <Link href={href} className={baseClasses}>
+      {children}
+    </Link>
+  ) : (
+    <button {...rest} className={baseClasses}>
       {children}
     </button>
   );
