e84d231047cbcd76fe00283b11867fea79a7fcb0
yemishi <daviddperera10@gmail.com>
2025-05-15 20:51:48 +0100

    modify like logic and it will change icon to redirect to login instead of just redirect the users to it

--- src/hooks/useLike.tsx
+++ src/hooks/useLike.tsx
@@ -1,39 +1,40 @@
 "use client";
 
 import { ErrorType } from "@/types/response";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "react-toastify";
 
 export default function useLike(songId: string, username?: string) {
-  const [isLoading, setIsLoading] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
+  const queryClient = useQueryClient();
 
-  const { refresh, push } = useRouter();
-
-  useEffect(() => {
+  const fetchData = async () => {
     if (!username) return;
-    const fetchData = async () => {
-      setIsLoading(true);
-      const data: boolean = await fetch(`/api/song/likedSong?username=${username}&songId=${songId}`).then((res) =>
-        res.json()
-      );
-      setIsLiked(data), setIsLoading(false);
-    };
-    fetchData();
-  }, [songId]);
+    const data = await fetch(`/api/song/likedSong?username=${username}&songId=${songId}`).then((res) => res.json());
+    setIsLiked(data);
+  };
+  const { isLoading } = useQuery({
+    queryKey: ["likedSong", songId, username],
+    queryFn: async () => await fetchData(),
+  });
 
   const toggleLike = async () => {
-    if (!username) return push("/login");
-    setIsLoading(true), setIsLiked(!isLiked);
+    const optimisticValue = !isLiked;
+    queryClient.setQueryData(["likedSong", songId, username], optimisticValue);
+
     const data: ErrorType = await fetch(`/api/song/likedSong`, {
       method: "PATCH",
       body: JSON.stringify({ songId, username }),
     }).then((res) => res.json());
-    if (data.error) return toast.error(data.message), setIsLoading(false), setIsLiked(!isLiked);
-    return refresh(), setIsLoading(false);
-  };
 
+    if (data.error) {
+      queryClient.setQueryData(["likedSong", songId, username], isLiked);
+      return toast.error(data.message);
+    }
+
+    queryClient.refetchQueries({ queryKey: ["likedSong", songId, username] });
+  };
   return {
     toggleLike,
     isLiked,
