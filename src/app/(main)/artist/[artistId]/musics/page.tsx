bfab707b9f76edb0bf01678092c64ca501ccfc95
yemishi <daviddperera10@gmail.com>
2025-05-16 16:46:46 +0100

    passing user info via props to songGridQuery component

--- src/app/(main)/artist/[artistId]/musics/page.tsx
+++ src/app/(main)/artist/[artistId]/musics/page.tsx
@@ -3,10 +3,12 @@ import { use } from "react";
 
 import { SongsGridQuery } from "@/components";
 import { PreviousPage } from "@/ui";
+import { useSession } from "next-auth/react";
 
 export default function Musics(props: { params: Promise<{ artistId: string }> }) {
   const params = use(props.params);
-
+  const { data } = useSession();
+  const username = data?.user.name;
   const { artistId } = params;
   return (
     <div className="flex flex-col gap-3">
@@ -15,7 +17,7 @@ export default function Musics(props: { params: Promise<{ artistId: string }> })
         <h1 className="font-kanit text-2xl ml-4">Musics</h1>
       </div>
       <div className="p-2 flex flex-col ">
-        <SongsGridQuery queryKey={["Songs", artistId]} url={`/api/song?artistId=${artistId}`} />
+        <SongsGridQuery username={username} queryKey={["Songs", artistId]} url={`/api/song?artistId=${artistId}`} />
       </div>
     </div>
   );
