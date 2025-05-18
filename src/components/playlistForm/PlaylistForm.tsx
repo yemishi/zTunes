5b0153e6b0cb3e09027035775ebfbea59b2e0a96
yemishi <daviddperera10@gmail.com>
2025-05-18 01:49:49 +0100

    now it handles playlist edit method

--- src/components/playlistForm/PlaylistForm.tsx
+++ src/components/playlistForm/PlaylistForm.tsx
@@ -3,30 +3,42 @@
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
 import { ChangeEvent, useEffect, useState } from "react";
-import { ErrorType } from "@/types/response";
 
 import Button from "@/ui/buttons/Button";
-import uploadImage from "@/firebase/handleImage";
+import uploadImage, { deleteImage } from "@/firebase/handleImage";
 import Input from "../../ui/inputs/Input";
 import InputFileImg from "../../ui/inputs/InputFileImg";
 import AddCategories from "../../ui/inputs/AddCategories";
 import useForm from "@/hooks/useForm";
+import ToggleCheck from "./toggleCheck/ToggleCheck";
+import { createPlaylist, updatePlaylist } from "./playlistFormActions";
 
 export default function PlaylistForm({
   username,
   onSuccess,
   onClose,
+  id,
+  formInfo,
 }: {
   username: string;
-  onSuccess?: () => void;
   onClose: () => void;
+  id?: string;
+  onSuccess?: () => void;
+  formInfo?: {
+    coverPhoto: string;
+    name: string;
+    desc?: string;
+    categories?: string[];
+    isPublic?: boolean;
+  };
 }) {
-  const [demoPhoto, setDemoPhoto] = useState<string>();
+  const [demoPhoto, setDemoPhoto] = useState(formInfo?.coverPhoto || "");
   const [isLoading, setIsLoading] = useState(false);
-  const [isPublic, setIsPublic] = useState(false);
+  const [isPublic, setIsPublic] = useState(!!formInfo?.isPublic);
   const [isAdmin, setIsAdmin] = useState(false);
-  const [isCategories, setIsCategories] = useState(false);
-  const [categories, setCategories] = useState<string[]>([]);
+  const [isCategories, setIsCategories] = useState(!!formInfo?.categories);
+  const [categories, setCategories] = useState(formInfo?.categories || []);
+
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetch(`/api/user?username=${username}`).then((res) => res.json());
@@ -44,11 +56,10 @@ export default function PlaylistForm({
     validateAll,
     values: { coverPhoto, desc, name },
   } = useForm<{ name: string; desc: string; coverPhoto: string | FileList }>({
-    name: { value: "", min: 1, max: 20 },
-    desc: { value: "" },
-    coverPhoto: { value: "" },
+    name: { value: formInfo?.name || "", min: 1, max: 20 },
+    desc: { value: formInfo?.desc || "" },
+    coverPhoto: { value: formInfo?.coverPhoto || "" },
   });
-
   const { refresh } = useRouter();
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -70,29 +81,31 @@ export default function PlaylistForm({
     if (!validateAll()) return;
     setIsLoading(true);
 
-    const imageUrl = await uploadImage(coverPhoto as FileList);
-    if (imageUrl.error) return toast.error(imageUrl.message), setIsLoading(false);
+    const fetchImg = async () => {
+      if (formInfo?.coverPhoto && formInfo.coverPhoto !== coverPhoto) await deleteImage(formInfo?.coverPhoto);
+      if (coverPhoto === demoPhoto) return { url: coverPhoto, error: false, message: "" };
+      const imageUrl = await uploadImage(coverPhoto as FileList);
+      return imageUrl;
+    };
+    const imgData = await fetchImg();
+    if (imgData.error) return toast.error(imgData.message), setIsLoading(false);
 
     const body = {
       title: name,
       username,
       isPublic,
       desc,
-      coverPhoto: imageUrl.url,
-      songs: [],
+      coverPhoto: imgData.url as string,
       officialCategories: isAdmin && isCategories && categories.length > 0 ? categories : null,
     };
-
-    const uploadPlaylist: ErrorType = await fetch("/api/playlist", {
-      method: "POST",
-      body: JSON.stringify(body),
-    }).then((res) => res.json());
-    const { error, message } = uploadPlaylist;
+    const fetchPlaylist = async () => (id ? await updatePlaylist(body, id) : await createPlaylist(body));
+    const data = await fetchPlaylist();
+    const { error, message } = data;
     if (error) return toast.error(message), setIsLoading(false);
     return toast(message), refresh(), onClose(), setIsLoading(false), onSuccess ? onSuccess() : null;
   };
   return (
-    <form onSubmit={onSubmit} className="gap-6 md:text-lg p-5 w-full h-full md:rounded-md text-black flex flex-col">
+    <form onSubmit={onSubmit} className="gap-6 md:text-lg p-5 w-full md:rounded-md text-black flex flex-col">
       <InputFileImg
         className="bg-black/70 mx-auto w-80 mb-6"
         isLoading={isLoading}
@@ -127,7 +140,7 @@ export default function PlaylistForm({
         ></textarea>
       </div>
 
-      <Toggle
+      <ToggleCheck
         desc={isPublic ? "Everyone can see this playlist" : "Only you can see this playlist"}
         isActive={isPublic}
         toggle={() => setIsPublic((prev) => !prev)}
@@ -135,8 +148,8 @@ export default function PlaylistForm({
       />
 
       {isAdmin && (
-        <div className="flex flex-col gap-7 sm:col-span-2 sm:ml-auto sm:mr-auto items-center">
-          <Toggle
+        <div className="flex flex-col w-full  gap-7 sm:col-span-2 sm:ml-auto sm:mr-auto items-center">
+          <ToggleCheck
             title="Official"
             desc={isCategories ? "This playlist will be official" : "This playlist will not be official"}
             isActive={isCategories}
@@ -151,38 +164,9 @@ export default function PlaylistForm({
           Back
         </Button>
         <Button disabled={isLoading} type="submit">
-          Create
+          {id ? "Update" : "Create"}
         </Button>
       </span>
     </form>
-  );
-}
-
-function Toggle({
-  isActive,
-  title,
-  desc,
-  toggle,
-}: {
-  isActive: boolean;
-  desc: string;
-  title: string;
-  toggle: () => void;
-}) {
-  return (
-    <div className="flex items-center justify-between sm:items-end sm:self-end w-full gap-5">
-      <div className="flex flex-col">
-        <span className="text-lg">{title}</span>
-        <span className="font-semibold">{desc}</span>
-      </div>
-
-      <span
-        onClick={toggle}
-        className={`w-14 h-7 rounded-full self-center relative after:absolute after:h-5/6 after:w-6 after:top-2/4 after:-translate-y-2/4 after:bg-white 
-    after:rounded-full duration-200 after:duration-200 cursor-pointer ${
-      isActive ? "bg-orange-400 after:left-2/4" : "bg-black/50 after:left-1"
-    }`}
-      />
-    </div>
   );
 }
