7804dc6e303a6f6cb6645fc60ae54bf908205e05
yemishi <daviddperera10@gmail.com>
2025-05-19 15:02:59 +0100

    improves ui spacing

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,7 +5,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
       <Sidebar />
-      <div className="pb-32 flex flex-col md:pb-20 md:ml-62 lg:ml-70 2xl:ml-78 3xl:ml-94 bg-black-700 h-full overflow-y-auto md:rounded-xl overflow-hidden">
+      <div className="pb-32 md:p-2 min-h-full md:pb-20 md:ml-62 lg:ml-70 2xl:ml-78 3xl:ml-94 bg-black-700 overflow-y-auto overflow-x-hidden">
         {children}
       </div>
       <UnderBar />
