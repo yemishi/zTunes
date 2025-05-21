4601f25ca79550ab79434a5ed21b10facd31732d
yemishi <daviddperera10@gmail.com>
2025-05-21 17:12:24 +0100

    adjust css style of profile pic

--- src/components/sidebar/userSessionPanel/SessionPanel.tsx
+++ src/components/sidebar/userSessionPanel/SessionPanel.tsx
@@ -10,12 +10,12 @@ export default function UserSessionPanel({ user }: { user?: { name: string; pict
         href="/account"
         className="flex gap-2 items-center p-2 rounded-r-lg rounded-l-3xl font-bold hover:bg-black-450 duration-150 active:bg-black"
       >
-        <Image src={user.picture} alt="User avatar" className="size-12 rounded-3xl" />
+        <Image src={user.picture} alt="User avatar" className="size-12 rounded-3xl object-cover" />
         <span className="line-clamp-1 mt-auto mb-2">{user.name.charAt(0).toUpperCase() + user.name.slice(1)}</span>
       </Link>
       <Logout
         animateLess
-        className="rounded-lg bg-transparent opacity-50 hover:bg-black-450 hover:opacity-100 duration-100 font-bold active:bg-black"
+        className="rounded-lg  bg-transparent opacity-50 hover:bg-black-450 hover:opacity-100 duration-100 font-bold active:bg-black"
       />
     </div>
   );
