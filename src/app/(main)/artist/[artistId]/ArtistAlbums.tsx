d7ec07854749873db71f012113fc055ff5c5ac0c
yemishi <daviddperera10@gmail.com>
2025-05-24 11:22:23 +0100

    replaced query method from useEffect to useQuery

--- src/app/(main)/artist/[artistId]/ArtistAlbums.tsx
+++ src/app/(main)/artist/[artistId]/ArtistAlbums.tsx
@@ -4,24 +4,15 @@ import ErrorWrapper from "@/components/errorWrapper/ErrorWrapper";
 import BundleOrganizer from "@/components/bundleGrid/BundleGrid";
 import BundleStackSkeleton from "@/components/skeletons/bundle/BundleStackSkeleton";
 import { BundleType } from "@/types/response";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 export default function ArtistAlbums({ artistId }: { artistId: string }) {
-  interface ResponseType extends Array<BundleType> {
-    error: false;
-    message: string;
-  }
-  const [data, setData] = useState<ResponseType>();
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    const fetchAlbums = async () => {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      const response: ResponseType = await fetch(`/api/album?artistId=${artistId}`).then((res) => res.json());
-      setData(response);
-      setIsLoading(false);
-    };
-    fetchAlbums();
-  }, [artistId]);
+  const queryFn = () => fetch(`/api/album?artistId=${artistId}`).then((res) => res.json());
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["artistAlbum", artistId],
+    refetchOnWindowFocus: false,
+    queryFn,
+  });
 
   if (isLoading) return <BundleStackSkeleton count={10} />;
   const albums = data ? data.filter((album: BundleType) => album.type.toLowerCase() === "album") : [];
@@ -30,7 +21,7 @@ export default function ArtistAlbums({ artistId }: { artistId: string }) {
   return (
     <>
       {data && (
-        <ErrorWrapper error={data.error} message={data.message}>
+        <ErrorWrapper error={data.error || isError} message={data.message}>
           {!!albums.length && <BundleOrganizer baseUrl="/album" title="Albums" props={albums} />}
 
           {!!singles.length && <BundleOrganizer baseUrl="/album" title="Singles" props={singles} />}
